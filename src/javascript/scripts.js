d02fbdd82f8a869c323f729fb6b2ed4ee296ceee
Chris King <chris@weckey.com>
2015-02-23 16:44:31 -0800

    Home:
    - added work tile hovers

---
+++ src/javascript/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     $(".").on("mouseenter", ".work-container", function(){						
+//         $(this).find('.overlay').fadeIn(300);
+//     }).on("mouseleave", ".work-container", function(){	
+//         $(this).find('.overlay').stop().fadeOut(300);
+//     })	
+// });
