bff5cb45fa841730798073c34a69ae44e2c695a5
Chris King <chris@weckey.com>
2015-03-27 14:17:42 -0700

    Work:
    - Added Scroll btn

--- public/javascript/all.js
+++ public/javascript/all.js
@@ -11511,10 +11511,16 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-// $(document).ready(function(){
-//     $(".").on("mouseenter", ".work-container", function(){						
-//         $(this).find('.overlay').fadeIn(300);
-//     }).on("mouseleave", ".work-container", function(){	
-//         $(this).find('.overlay').stop().fadeOut(300);
-//     })	
-// });
+$(document).ready(function(){
+    // $(".").on("mouseenter", ".work-container", function(){						
+    //     $(this).find('.overlay').fadeIn(300);
+    // }).on("mouseleave", ".work-container", function(){	
+    //     $(this).find('.overlay').stop().fadeOut(300);
+    // })	
+
+	$('#btn-scroll').click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({scrollTop: $('.hero').outerHeight() - $('header').outerHeight()}, 800);
+	})
+	
+});
--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,7 +1,13 @@
-// $(document).ready(function(){
-//     $(".").on("mouseenter", ".work-container", function(){						
-//         $(this).find('.overlay').fadeIn(300);
-//     }).on("mouseleave", ".work-container", function(){	
-//         $(this).find('.overlay').stop().fadeOut(300);
-//     })	
-// });
+$(document).ready(function(){
+    // $(".").on("mouseenter", ".work-container", function(){						
+    //     $(this).find('.overlay').fadeIn(300);
+    // }).on("mouseleave", ".work-container", function(){	
+    //     $(this).find('.overlay').stop().fadeOut(300);
+    // })	
+
+	$('#btn-scroll').click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({scrollTop: $('.hero').outerHeight() - $('header').outerHeight()}, 800);
+	})
+	
+});
