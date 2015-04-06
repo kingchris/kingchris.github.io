06e85ff7f943091e7e5a60a03797adac8a9833a8
Chris King <chris@weckey.com>
2015-04-06 10:39:08 -0700

    Project:
    - Added vertical responsiveness to hero image

--- public/javascript/all.js
+++ public/javascript/all.js
@@ -11512,11 +11512,11 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 $(document).ready(function(){
-    // $(".").on("mouseenter", ".work-container", function(){						
-    //     $(this).find('.overlay').fadeIn(300);
-    // }).on("mouseleave", ".work-container", function(){	
-    //     $(this).find('.overlay').stop().fadeOut(300);
-    // })	
+	// $(".").on("mouseenter", ".work-container", function(){						
+	//     $(this).find('.overlay').fadeIn(300);
+	// }).on("mouseleave", ".work-container", function(){	
+	//     $(this).find('.overlay').stop().fadeOut(300);
+	// })	
 
 	
 	// Hero Scroll Button
@@ -11526,4 +11526,30 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: $('#hero').outerHeight() - $('header').outerHeight()}, 800);
 	})
 	
+	
+	// Adjust #hero-img size based on screen height on window resize
+	
+	$(function(){
+		$(window).bind("resize",function(){
+			if($(this).height() < 901){
+				$('#hero-img').removeClass('col-lg-10 col-lg-offset-1').addClass('col-md-8 col-md-offset-2')
+			}
+			else{
+				$('#hero-img').removeClass('col-md-8 col-md-offset-2').addClass('col-lg-10 col-lg-offset-1')
+			}
+		})
+	})
+	
 });
+
+
+// Adjust #hero-img size based on screen height on window load
+
+$(window).load(function(){
+	if($(this).height() < 901){
+		$('#hero-img').removeClass('col-lg-10 col-lg-offset-1').addClass('col-md-8 col-md-offset-2')
+	}
+	else{
+		$('#hero-img').removeClass('col-md-8 col-md-offset-2').addClass('col-lg-10 col-lg-offset-1')
+	}
+})
--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    // $(".").on("mouseenter", ".work-container", function(){						
-    //     $(this).find('.overlay').fadeIn(300);
-    // }).on("mouseleave", ".work-container", function(){	
-    //     $(this).find('.overlay').stop().fadeOut(300);
-    // })	
+	// $(".").on("mouseenter", ".work-container", function(){						
+	//     $(this).find('.overlay').fadeIn(300);
+	// }).on("mouseleave", ".work-container", function(){	
+	//     $(this).find('.overlay').stop().fadeOut(300);
+	// })	
 
 	
 	// Hero Scroll Button
@@ -13,4 +13,30 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: $('#hero').outerHeight() - $('header').outerHeight()}, 800);
 	})
 	
+	
+	// Adjust #hero-img size based on screen height on window resize
+	
+	$(function(){
+		$(window).bind("resize",function(){
+			if($(this).height() < 901){
+				$('#hero-img').removeClass('col-lg-10 col-lg-offset-1').addClass('col-md-8 col-md-offset-2')
+			}
+			else{
+				$('#hero-img').removeClass('col-md-8 col-md-offset-2').addClass('col-lg-10 col-lg-offset-1')
+			}
+		})
+	})
+	
 });
+
+
+// Adjust #hero-img size based on screen height on window load
+
+$(window).load(function(){
+	if($(this).height() < 901){
+		$('#hero-img').removeClass('col-lg-10 col-lg-offset-1').addClass('col-md-8 col-md-offset-2')
+	}
+	else{
+		$('#hero-img').removeClass('col-md-8 col-md-offset-2').addClass('col-lg-10 col-lg-offset-1')
+	}
+})
